a594144df295eb2c3a3d1bc3ff96bee22c6386df
Felipe Henrique <fheehenrique@gmail.com>
2022-05-20 15:49:48 -0300

    New app to BMI Calc

---
+++ README.md
@@ -0,0 +1,2 @@
+# learning_flutter
+ This repository is dedicated to learning flutter
---
+++ imc_calc/lib/main.dart
@@ -0,0 +1,20 @@
+import 'screens/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'BMI Calculator',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.yellow,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
